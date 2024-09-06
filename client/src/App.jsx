8cc1497d05541709f28eab81024a18bd80773b4a
Houssine <ekhay1@outlook.com>
2024-09-06 12:59:13 -0400

    refactored the app.jsx to use graphql and add apolloclient and provider to apps.jsx, install appollo client and qraphql packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
 
-import Navbar from './components/Navbar';
+// requiring from apollo/client
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { Outlet } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+//instantiate client to use to access data in the server
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    // adding apolloprovider that allow all childern inside to access data from the server
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
