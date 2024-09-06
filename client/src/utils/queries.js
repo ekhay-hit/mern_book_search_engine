e465e9f1a91b5d8c254094b85ebb4ed949cb36ab
Houssine <ekhay1@outlook.com>
2024-09-06 13:25:50 -0400

    added the queries.js and mutation.js in the utils client side and adde query for getting a singel a user

---
+++ client/src/utils/queries.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+// sigleUser you can laterley copy itform the GUI
+export const QUERY_SINGLE_USER = qgl`
+query singleUser($singleId: ID!) {
+    users(userId : $profileId){
+    _id
+    name
+    }
+}
+`;
