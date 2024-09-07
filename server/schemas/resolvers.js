9882a7bb2c3d429385f2599cde107154130fba55
Houssine <ekhay1@outlook.com>
2024-09-06 22:05:54 -0400

    added login and signup mutation in the backend resolver file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,9 +12,42 @@ const resolvers = {
 
     // return book by its id
     user: async (_, args) => {
-      return await User.findById(args.id);
+       await User.findById(args.id);
     },
   },
+
+
+Mutation:{
+
+     // Login mutation
+   login: async(_,{email, password})=>{
+     const profile = User.findOne({email});
+     if(!profile){
+         // return res.status(404).json("No profile found");
+         throw AuthenticationError;
+     }
+     const passwordIsValid = await profile.isCorrectPassword(password);
+     if(!passwordIsValid){
+         // throw new Error("Password is not valid");
+         throw AuthenticationError;
+         
+     }
+     // const token = signToken(profile)
+     // return {token, profile};
+     return profile;
+   }
+
+   // Singup Mutation
+   signUp: async (parent, {name,email, password})=>{
+        const user = await User.create({name,email, password});
+       const token = signToken(user);
+       return {token, profile}
+   }
+
+
+
+
+}
 };
 
 // export resolvers
